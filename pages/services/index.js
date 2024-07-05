ee5270da0a483d08fac74c64277c02201ee6867f
tii_ui <suontii2023@gmail.com>
2024-07-05 10:35:06 +0700

    Commit

--- pages/services/index.js
+++ pages/services/index.js
@@ -17,18 +17,33 @@ const Services = () => {
                    <div className='flex flex-col lg:flex-row gap-x-8'>
                         {/* text */}
                          <div className='text-center flex lg:w-[30vw] flex-col lg:text-left mb-4 lg:mb-0'>
-                             <h2 className='h2 lg:mt-8'>
-                                 My Services <span className='text-accent'>.</span>
-                             </h2>
-                             <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-                               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                               industry standard dummy text ever since the 1500s.
-                             </p>
+                            <motion.h2
+                              variants={fadeIn('up', 0.2)}
+                              initial='hidden'
+                              animate='show'
+                              exit='show'
+                              className='h2 lg:mt-8'>
+                                My Services <span className='text-accent'>.</span>
+                             </motion.h2>
+                             <motion.p
+                                variants={fadeIn('up', 0.4)}
+                                initial='hidden'
+                                animate='show'
+                                exit='show'
+                                className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                                industry standard dummy text ever since the 1500s.
+                             </motion.p>
                          </div>
                         {/* Slider */}
-                        <div className="w-full cursor-pointer relative z-30 h-fit lg:max-w-[65%%]">
+                        <motion.div
+                            variants={fadeIn('up', 0.6)}
+                            initial='hidden'
+                            animate='show'
+                            exit='show'
+                            className="w-full cursor-pointer relative z-30 h-fit lg:max-w-[65%%]">
                            <ServiceSlider/>
-                        </div>
+                        </motion.div>
                    </div>
                </div>
                <Bulb/>
