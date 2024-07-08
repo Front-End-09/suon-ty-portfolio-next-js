2894528b2310f872fa9517eff0fce6cf3ab39139
TiiTuy2 <suonty2023@gmail.com>
2024-07-08 23:11:04 +0700

    Ehe End

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
 module.exports = {
+    output: 'export',
   distDir: 'out',
   basePath: 'suon-ty-portfolio-next-js',
   assetPrefix: '/suon-ty-portfolio-next-js/',
