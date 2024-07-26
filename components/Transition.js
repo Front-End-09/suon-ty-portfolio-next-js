2dfbbfb05cf43910ac1aa5bb9d83cdd8b2e19cd7
TiiTuy2 <suonty2023@gmail.com>
2024-07-26 20:58:03 +0700

    Remove number

--- components/Transition.js
+++ components/Transition.js
@@ -23,7 +23,6 @@ const Transition = () => {
         animate="animate" exit="exit"
         transition={{delay: 0.2,duration: 0.6, ease: 'easeInOut'}}
         >
-          1
       </motion.div>
       <motion.div className='fixed top-0 bottom-0 right-full w-screen h-full z-20 bg-[#3b2d71]'
         variants={transitionvariants}
@@ -31,7 +30,6 @@ const Transition = () => {
         animate="animate" exit="exit"
         transition={{delay: 0.4,duration: 0.6, ease: 'easeInOut'}}
         >
-          1
       </motion.div>
       <motion.div className='fixed top-0 bottom-0 right-full w-screen h-full z-10 bg-[#4b3792]'
         variants={transitionvariants}
@@ -39,7 +37,6 @@ const Transition = () => {
         animate="animate" exit="exit"
         transition={{delay: 0.6,duration: 0.6, ease: 'easeInOut'}}
         >
-          1
       </motion.div>
     </>
   );
